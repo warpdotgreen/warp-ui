45a935884c8b3c21d53712ace5383b54065aaf5a
yakuhito <y@kuhi.to>
2024-04-07 18:32:59 +0200

    this already looks better, but still isn't it

--- src/app/ui/page.tsx
+++ src/app/ui/page.tsx
@@ -13,8 +13,8 @@ export default function Home() {
 
       <div className="max-w-md mx-auto py-8">
         <div className="mx-auto border-zinc-700 rounded-lg border p-6 bg-zinc-900">
-          <form className="space-y-8">
-            <div className="space-y-3">
+          <form className="space-y-6">
+            <div className="space-y-2">
               <div className="flex justify-right items-center">
                 <label className="text-zinc-300 text-xl font-medium pr-4">Token</label>
                 <select className="px-2 py-2 border border-zinc-700 rounded bg-zinc-800 text-zinc-100 outline-none">
@@ -27,69 +27,59 @@ export default function Home() {
                 <BlockchainDropdown />
                 <button
                   type="button"
-                  className="mx-2 text-green-500 border-2 border-green-500 rounded-full px-3"
+                  className="mx-2 p-2 text-zinc-300 hover:bg-zinc-700 rounded-xl"
                 >
-                  →
+                  <ChangeArrow />
                 </button>
                 <BlockchainDropdown />
               </div>
               <input
                 type="text"
                 placeholder="Amount"
-                className="w-full px-3 py-2 border-2 border-green-500 rounded bg-gray-900 text-green-500 placeholder-green-500 text-lg"
+                className="w-full px-2 py-2 border border-zinc-700 rounded outline-none bg-zinc-800 text-zinc-300 placeholder-zinc-500 text-lg"
                 pattern="^\d*(\.\d{0,8})?$"
               />
               <input
                 type="text"
                 placeholder="Receive Address"
-                className="w-full px-3 py-2 border-2 border-green-500 rounded bg-gray-900 text-green-500 placeholder-green-500 text-lg"
+                className="w-full px-2 py-2 border border-zinc-700 rounded outline-none bg-zinc-800 text-zinc-300 placeholder-zinc-500 text-lg"
               />
             </div>
 
-            <button
-              type="submit"
-              className="w-full py-2 border-2 border-green-500 rounded bg-black text-green-500 hover:bg-green-500 hover:text-black transition-colors duration-300"
-            >
-              Bridge
-            </button>
+            <div className="flex justify-center">
+              <button
+                type="submit"
+                className="w-64 p-2 border border-zinc-700 rounded-3xl bg-green-500 font-medium text-zinc-950 hover:bg-green-700 transition-colors duration-300"
+              >
+                Bridge
+              </button>
+            </div>
           </form>
         </div>
       </div>
 
       <div className="flex justify-center pb-4">
-        <p className="text-green-500">Docs | Twitter | Terms</p>
+        <p className="text-zinc-300">Docs | Twitter | Terms</p>
       </div>
-    </div>
-  );
-}
-
-function InputElement() {
-  return (
-    <div className="flex items-center space-x-2 px-3 py-4 border-2 border-green-500 rounded">
-      <input
-        type="text"
-        placeholder="Amount"
-        className="flex-shrink min-w-0 outline-none bg-gray-900 text-green-500 placeholder-green-500 text-lg"
-      />
-      <select className="flex-grow-0 px-3 py-2 border-2 border-green-500 rounded bg-gray-900 text-green-500 outline-none">
-        <option>Chia</option>
-        <option>Base</option>
-        <option>Ethereum</option>
-      </select>
-      <select className="flex-grow-0 px-3 py-2 border-2 border-green-500 rounded bg-gray-900 text-green-500 outline-none">
-        <option>ETH</option>
-        <option>USDC</option>
-      </select>
     </div>
   );
 }
 
 function BlockchainDropdown() {
   return (
-    <select className="px-3 py-2 border-2 border-green-500 rounded bg-gray-900 text-green-500 outline-none">
+    <select className="px-2 py-2 border flex-1 border-zinc-700 rounded bg-zinc-800 text-zinc-300 outline-none">
       <option>Ethereum</option>
       <option>Base</option>
       <option>Chia</option>
     </select>
+  );
+}
+
+// https://heroicons.com/ 
+function ChangeArrow() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M7.5 21 3 16.5m0 0L7.5 12M3 16.5h13.5m0-13.5L21 7.5m0 0L16.5 12M21 7.5H7.5" />
+    </svg>
   );
 }
