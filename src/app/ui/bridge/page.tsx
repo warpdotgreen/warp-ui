30eab06ef0d8f81a7e79fa51fceb7550ff582bda
yakuhito <y@kuhi.to>
2024-04-10 13:23:08 +0200

    fix svg errors

--- src/app/ui/bridge/page.tsx
+++ src/app/ui/bridge/page.tsx
@@ -1,9 +1,12 @@
 "use client";
 
+import { useSearchParams } from "next/navigation";
 import { ChiaWalletContext } from "../chia_wallet_context";
 import { MultiStepForm } from "./MultiStepForm";
 
 export default function BridgingFirstStep() {
+  const searchParams = useSearchParams();
+  const step = parseInt(searchParams.get("step")!);
 
   const sourceChain = "Base";
   const destinationChain = "Chia";
@@ -15,7 +18,7 @@ export default function BridgingFirstStep() {
           <MultiStepForm
             sourceChain={sourceChain}
             destinationChain={destinationChain}
-            activeStep={1}
+            activeStep={step}
             >
               <p>Hello, sir!</p>
           </MultiStepForm>
