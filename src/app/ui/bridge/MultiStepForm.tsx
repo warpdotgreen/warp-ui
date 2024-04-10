0aa39b648a8aa24947d589e2d01a87e87e81b6ba
yakuhito <y@kuhi.to>
2024-04-10 13:13:22 +0200

    extract thing to its own component

---
+++ src/app/ui/bridge/MultiStepForm.tsx
@@ -0,0 +1,58 @@
+export function MultiStepForm({
+  sourceChain,
+  destinationChain,
+  activeStep,
+  children,
+}: {
+  sourceChain: string,
+  destinationChain: string,
+  activeStep: number,
+  children: React.ReactNode,
+}) {
+  const steps = [
+    { text: `Send tokens on ${sourceChain}`, icon: <FeeIcon />, iconText: "Fee" },
+    { text: "Wait for transaction confirmation", icon: <ClockIcon />, iconText: "18 min" },
+    { text: `Claim tokens on ${destinationChain}`, icon: <FeeIcon />, iconText: "Fee" },
+  ]
+
+  return (
+    <div className="max-w-lg w-full mx-auto py-8">
+      <div className="mx-auto border-zinc-700 rounded-lg border p-6 bg-zinc-900 space-y-6">
+        {steps.map((step, index) => {
+          return (
+            <div key={index} className={`space-y-2 ${activeStep - 1 == index ? 'text-zinc-100' : 'text-zinc-500'}`}>
+              <div className="flex justify-between items-center">
+                <p className="text-lg font-medium">{index + 1}. {step.text}</p>
+                <div className="flex">
+                  {step.icon}
+                  <span className="ml-1">{step.iconText}</span>
+                </div>
+              </div>
+              <div className="mx-6">
+                {activeStep - 1 == index && children}
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+// https://heroicons.com/
+function ClockIcon() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+    </svg>
+  );
+}
+
+// https://heroicons.com/
+function FeeIcon() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M20.25 6.375c0 2.278-3.694 4.125-8.25 4.125S3.75 8.653 3.75 6.375m16.5 0c0-2.278-3.694-4.125-8.25-4.125S3.75 4.097 3.75 6.375m16.5 0v11.25c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125V6.375m16.5 0v3.75m-16.5-3.75v3.75m16.5 0v3.75C20.25 16.153 16.556 18 12 18s-8.25-1.847-8.25-4.125v-3.75m16.5 0c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125" />
+    </svg>
+  );
+}
--- src/app/ui/bridge/page.tsx
+++ src/app/ui/bridge/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { ChiaWalletContext } from "../chia_wallet_context";
+import { MultiStepForm } from "./MultiStepForm";
 
 export default function BridgingFirstStep() {
 
@@ -11,64 +12,15 @@ export default function BridgingFirstStep() {
     <ChiaWalletContext.Consumer>
       {(chiaWalletContext) => {
         return (
-          <div className="max-w-lg w-full mx-auto py-8">
-            <div className="mx-auto border-zinc-700 rounded-lg border p-6 bg-zinc-900">
-              <div className="space-y-6">
-                <div className={`space-y-2 ${true ? 'text-zinc-100' : 'text-zinc-500'}`}>
-                    <div className="flex justify-between items-center">
-                      <p className="text-lg font-medium">1. Send tokens on {sourceChain}</p>
-                      <div className="flex">
-                        <FeeIcon />
-                        <span className="ml-1">Fee</span>
-                      </div>
-                    </div>
-                    <div className="mx-6">
-                      {true && (
-                        <p className="text-zinc-100">Hello world!</p>
-                      )}
-                    </div>
-                  </div>
-                  <div className={`space-y-2 ${false ? 'bg-zinc-800 text-zinc-100' : 'text-zinc-500'}`}>
-                    <div className="flex justify-between items-center">
-                      <p className="text-lg font-medium">2. Wait for transaction confirmation</p>
-                      <div className="flex">
-                        <ClockIcon />
-                        <span className="ml-1">18 min</span>
-                      </div>
-                    </div>
-                  </div>
-                  <div className={`space-y-2 ${false ? 'bg-zinc-800 text-zinc-100' : 'text-zinc-500'}`}>
-                    <div className="flex justify-between items-center">
-                      <p className="text-lg font-medium">3. Claim tokens on {destinationChain}</p>
-                      <div className="flex">
-                        <FeeIcon />
-                        <span className="ml-1">Fee</span>
-                      </div>
-                    </div>
-                  </div>
-              </div>
-            </div>
-          </div>
+          <MultiStepForm
+            sourceChain={sourceChain}
+            destinationChain={destinationChain}
+            activeStep={1}
+            >
+              <p>Hello, sir!</p>
+          </MultiStepForm>
         );
       }}
     </ChiaWalletContext.Consumer>
-  );
-}
-
-// https://heroicons.com/
-function ClockIcon() {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
-      <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-    </svg>
-  );
-}
-
-// https://heroicons.com/
-function FeeIcon() {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
-      <path stroke-linecap="round" stroke-linejoin="round" d="M20.25 6.375c0 2.278-3.694 4.125-8.25 4.125S3.75 8.653 3.75 6.375m16.5 0c0-2.278-3.694-4.125-8.25-4.125S3.75 4.097 3.75 6.375m16.5 0v11.25c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125V6.375m16.5 0v3.75m-16.5-3.75v3.75m16.5 0v3.75C20.25 16.153 16.556 18 12 18s-8.25-1.847-8.25-4.125v-3.75m16.5 0c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125" />
-    </svg>
   );
 }
