2d9ddda1c3bbfe6201553a80a6528c899789d44d
Jonathan <jonathan@langtons.me>
2024-05-02 19:26:45 +0100

    Increase reading contrast

--- src/app/bridge/components/TopBanner.tsx
+++ src/app/bridge/components/TopBanner.tsx
@@ -4,8 +4,8 @@ function TopBanner() {
 
   if (TESTNET) {
     return (
-      <div className="text-yellow-300 bg-destructive text-center w-full py-2 px-4 font-bold">
-        <p>This is a testnet interface. Do not use mainnet funds. Make sure Goby is on <span className="text-blue-500">testnet11</span> and your Ethereum wallet is on <span className="text-blue-500">Sepolia</span> or <span className="text-blue-500">Base Sepolia</span>.</p>
+      <div className="text-yellow-300 bg-destructive sm:text-center w-full py-2 px-4 font-medium">
+        <p>This is a testnet interface. Do not use mainnet funds. Make sure Goby is on <span className="text-blue-800">testnet11</span> and your Ethereum wallet is on <span className="text-blue-800">Sepolia</span> or <span className="text-blue-800">Base Sepolia</span>.</p>
       </div>
     )
   }
