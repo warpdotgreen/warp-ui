29f4927264c4622ffdbda3d13e549663be6515bf
Jonathan <jonathan@langtons.me>
2024-05-02 19:40:06 +0100

    Add dark mode colours

--- src/app/bridge/components/TopBanner.tsx
+++ src/app/bridge/components/TopBanner.tsx
@@ -5,7 +5,7 @@ function TopBanner() {
   if (TESTNET) {
     return (
       <div className="text-yellow-300 bg-destructive sm:text-center w-full py-2 px-4 font-medium">
-        <p>This is a testnet interface. Do not use mainnet funds. Make sure Goby is on <span className="text-blue-800">testnet11</span> and your Ethereum wallet is on <span className="text-blue-800">Sepolia</span> or <span className="text-blue-800">Base Sepolia</span>.</p>
+        <p>This is a testnet interface. Do not use mainnet funds. Make sure Goby is on <span className="text-blue-800 dark:text-blue-300">testnet11</span> and your Ethereum wallet is on <span className="text-blue-800 dark:text-blue-300">Sepolia</span> or <span className="text-blue-800 dark:text-blue-300">Base Sepolia</span>.</p>
       </div>
     )
   }
