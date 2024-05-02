81a58058f1c698e21ac82900ca44cfc51baa470a
Jonathan <jonathan@langtons.me>
2024-05-02 18:00:42 +0100

    Add new Chia wallet modal

--- src/app/bridge/layout.tsx
+++ src/app/bridge/layout.tsx
@@ -1,9 +1,9 @@
-import Link from "next/link";
-import { ClientProvider } from "./ClientProvider";
-import { ChiaWalletButton } from "./ChiaWalletButton";
-import { TESTNET } from "./config";
-import EthereumWalletButton from "./EthereumWalletButton";
-import { Suspense } from "react";
+import Link from "next/link"
+import { ClientProvider } from "./ClientProvider"
+import { TESTNET } from "./config"
+import EthereumWalletButton from "./EthereumWalletButton"
+import { Suspense } from "react"
+import ChiaWalletModal from "./ChiaWalletModal"
 
 export default function BridgeUILayout({
   children,
@@ -21,7 +21,7 @@ export default function BridgeUILayout({
             <div className="text-zinc-300 text-2xl font-normal pt-1">Bridging Interface</div>
             <div className="flex space-x-2">
               <EthereumWalletButton />
-              <ChiaWalletButton />
+              <ChiaWalletModal />
             </div>
           </div>
         </div>
@@ -41,20 +41,20 @@ export default function BridgeUILayout({
                 className="px-2 underline hover:text-zinc-100"
               >
                 FAQ
-              </Link> | 
-               <Link
+              </Link> |
+              <Link
                 href="/bridge/assets"
                 className="px-2 underline hover:text-zinc-100"
               >
                 Supported Assets
-              </Link> | 
+              </Link> |
               <Link
                 href="https://docs.warp.green/developers/introduction"
                 target="_blank"
                 className="px-2 underline hover:text-zinc-100"
               >
                 Docs
-              </Link> | 
+              </Link> |
               <Link
                 href="https://twitter.com/warpdotgreen"
                 target="_blank"
@@ -79,7 +79,7 @@ export default function BridgeUILayout({
         </div>
       </div>
     </ClientProvider>
-  );
+  )
 }
 
 // https://heroicons.com/ 
@@ -88,5 +88,5 @@ function Sparkles() {
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
       <path strokeLinecap="round" strokeLinejoin="round" d="M9.813 15.904 9 18.75l-.813-2.846a4.5 4.5 0 0 0-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 0 0 3.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 0 0 3.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 0 0-3.09 3.09ZM18.259 8.715 18 9.75l-.259-1.035a3.375 3.375 0 0 0-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 0 0 2.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 0 0 2.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 0 0-2.456 2.456ZM16.894 20.567 16.5 21.75l-.394-1.183a2.25 2.25 0 0 0-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 0 0 1.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 0 0 1.423 1.423l1.183.394-1.183.394a2.25 2.25 0 0 0-1.423 1.423Z" />
     </svg>
-  );
+  )
 }
