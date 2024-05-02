b27876774513e1b85e9e68bc13ac6d3f18d2d283
Jonathan <jonathan@langtons.me>
2024-05-02 19:22:21 +0100

    Extract footer out to component

--- src/app/bridge/layout.tsx
+++ src/app/bridge/layout.tsx
@@ -1,7 +1,7 @@
-import Link from "next/link"
 import { ClientProvider } from "./ClientProvider"
 import { Suspense } from "react"
 import Header from "./components/Header"
+import Footer from "./components/Footer"
 
 export default function BridgeUILayout({
   children,
@@ -10,70 +10,15 @@ export default function BridgeUILayout({
 }) {
   return (
     <ClientProvider>
-      <div className="bg-zinc-950 min-h-screen flex flex-col justify-between">
+      <div className="bg-black min-h-screen flex flex-col justify-between">
         <Header />
 
         <Suspense>
           {children}
         </Suspense>
 
-        {/* footer */}
-
-        <div className="flex justify-center text-zinc-300 pb-3">
-          <div className="flex-col">
-            <div>
-              <Link
-                href="https://docs.warp.green/"
-                target="_blank"
-                className="px-2 underline hover:text-zinc-100"
-              >
-                FAQ
-              </Link> |
-              <Link
-                href="/bridge/assets"
-                className="px-2 underline hover:text-zinc-100"
-              >
-                Supported Assets
-              </Link> |
-              <Link
-                href="https://docs.warp.green/developers/introduction"
-                target="_blank"
-                className="px-2 underline hover:text-zinc-100"
-              >
-                Docs
-              </Link> |
-              <Link
-                href="https://twitter.com/warpdotgreen"
-                target="_blank"
-                className="px-2 underline hover:text-zinc-100"
-              >
-                Twitter
-              </Link> |
-              <Link
-                href="https://github.com/warpdotgreen"
-                target="_blank"
-                className="pl-2 underline hover:text-zinc-100"
-              >
-                GitHub
-              </Link>
-            </div>
-            <div className="flex justify-center pt-3">
-              <Sparkles />
-              <span className="pl-1">powered by</span>
-              <Link href="https://warp.green" className="text-green-500 hover:text-green-300 pl-1 pr-2 font-medium underline">warp.green</Link>
-            </div>
-          </div>
-        </div>
+        <Footer />
       </div>
     </ClientProvider>
-  )
-}
-
-// https://heroicons.com/ 
-function Sparkles() {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-      <path strokeLinecap="round" strokeLinejoin="round" d="M9.813 15.904 9 18.75l-.813-2.846a4.5 4.5 0 0 0-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 0 0 3.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 0 0 3.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 0 0-3.09 3.09ZM18.259 8.715 18 9.75l-.259-1.035a3.375 3.375 0 0 0-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 0 0 2.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 0 0 2.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 0 0-2.456 2.456ZM16.894 20.567 16.5 21.75l-.394-1.183a2.25 2.25 0 0 0-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 0 0 1.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 0 0 1.423 1.423l1.183.394-1.183.394a2.25 2.25 0 0 0-1.423 1.423Z" />
-    </svg>
   )
 }
