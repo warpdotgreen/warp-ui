21a361097b82dcd50470a7b68388070d5de887e0
Jonathan <jonathan@langtons.me>
2024-05-02 18:55:53 +0100

    Make prettier

--- src/app/bridge/ChiaWalletModal.tsx
+++ src/app/bridge/ChiaWalletModal.tsx
@@ -2,21 +2,23 @@
 import React from 'react'
 import { useWallet } from './ChiaWalletManager/WalletContext'
 import { walletConfigs } from './ChiaWalletManager/wallets'
+import { Button } from '@/components/ui/button'
+import { cn } from '@/lib/utils'
 
 const ChiaWalletModal: React.FC = () => {
   const { connectWallet, disconnectWallet, walletConnected, address } = useWallet()
 
   return (
     <div className='text-white'>
-      <h2>Connect to a Wallet</h2>
-      <p>Address: {address}</p>
-      {walletConfigs.map(wallet => (
-        <button className='border m-2' key={wallet.id} onClick={() => connectWallet(wallet.id)}>
-          Connect {wallet.name}
-        </button>
-      ))}
-      <button onClick={disconnectWallet} disabled={!walletConnected}>Disconnect</button>
-      {walletConnected ? <p className='text-red-600 font-medium'>Connected to: {walletConnected}</p> : <p>No wallet connected</p>}
+
+      <div className='flex gap-2'>
+        {walletConfigs.map(wallet => (
+          <Button key={wallet.id} disabled={wallet.id === walletConnected} className={cn('min-w-44', wallet.id === walletConnected && 'bg-green-800')} onClick={() => connectWallet(wallet.id)}>
+            {wallet.id === walletConnected ? `${address?.slice(0, 5)}...${address?.slice(-5)}` : `Connect To ${wallet.name}`}
+          </Button>
+        ))}
+        <Button onClick={disconnectWallet} disabled={!walletConnected}>Disconnect</Button>
+      </div>
     </div>
   )
 }
