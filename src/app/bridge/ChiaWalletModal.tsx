9bb81b333064b2fdc4b1a537e7ec70ab23c61df5
Jonathan <jonathan@langtons.me>
2024-05-02 18:57:59 +0100

    Make code prettier

--- src/app/bridge/ChiaWalletModal.tsx
+++ src/app/bridge/ChiaWalletModal.tsx
@@ -13,8 +13,19 @@ const ChiaWalletModal: React.FC = () => {
 
       <div className='flex gap-2'>
         {walletConfigs.map(wallet => (
-          <Button key={wallet.id} disabled={wallet.id === walletConnected} className={cn('min-w-44', wallet.id === walletConnected && 'bg-green-800')} onClick={() => connectWallet(wallet.id)}>
-            {wallet.id === walletConnected ? `${address?.slice(0, 5)}...${address?.slice(-5)}` : `Connect To ${wallet.name}`}
+          <Button
+            key={wallet.id}
+            disabled={wallet.id === walletConnected}
+            className={cn('min-w-44', wallet.id === walletConnected && 'bg-green-800')}
+            onClick={() => connectWallet(wallet.id)}
+          >
+            {
+              wallet.id === walletConnected
+                ?
+                `${address?.slice(0, 5)}...${address?.slice(-5)}`
+                :
+                `Connect To ${wallet.name}`
+            }
           </Button>
         ))}
         <Button onClick={disconnectWallet} disabled={!walletConnected}>Disconnect</Button>
