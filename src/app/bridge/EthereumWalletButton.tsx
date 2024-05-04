2225c94b3862e9cfdfaa92ffbdffb934e800f81f
Jonathan <jonathan@langtons.me>
2024-05-04 12:01:18 +0100

    Auto formatter

--- src/app/bridge/EthereumWalletButton.tsx
+++ src/app/bridge/EthereumWalletButton.tsx
@@ -1,15 +1,15 @@
-"use client";
+"use client"
 
 declare global {
   namespace JSX {
     interface IntrinsicElements {
-      "w3m-button": any;
+      "w3m-button": any
     }
   }
 }
 
 export default function EthereumWalletButton() {
   return (
-    <w3m-button balance='hide' label="Connect Ethereum Wallet"/>
-  );
+    <w3m-button balance='hide' label="Connect Ethereum Wallet" />
+  )
 }
