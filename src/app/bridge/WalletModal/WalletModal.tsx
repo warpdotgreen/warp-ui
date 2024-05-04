134abaf780174b0912b28b6fff1ceb6ab5ac3b48
Jonathan <jonathan@langtons.me>
2024-05-04 12:01:00 +0100

    Create wallet modal to house chia & eth wallets

--- src/app/bridge/WalletModal/WalletModal.tsx
+++ src/app/bridge/WalletModal/WalletModal.tsx
@@ -1,8 +1,36 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import ChiaWalletButtons from "../ChiaWalletButtons"
+import EthereumWalletButton from "../EthereumWalletButton"
+
+
 function WalletModal() {
   return (
-    <div>
+    <Dialog>
+      <DialogTrigger>Connect Wallet</DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Connect Wallet</DialogTitle>
+          <DialogDescription>
+            Connect a wallet for Chia & Ethereum chains
+          </DialogDescription>
+        </DialogHeader>
 
-    </div>
+        <p className="border-b">Chia</p>
+        <ChiaWalletButtons />
+
+        <p className="border-b">Ethereum</p>
+        <EthereumWalletButton />
+
+      </DialogContent>
+    </Dialog>
+
   )
 }
 
