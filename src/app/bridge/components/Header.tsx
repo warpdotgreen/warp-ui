2e6b02ea81be6513c751c0c106f53de89a942406
Jonathan <jonathan@langtons.me>
2024-05-04 12:08:31 +0100

    Responsive improvements & account for new walletmodal component

--- src/app/bridge/components/Header.tsx
+++ src/app/bridge/components/Header.tsx
@@ -1,20 +1,15 @@
-import ChiaWalletModal from "../ChiaWalletModal"
+import ChiaWalletButtons from "../ChiaWalletButtons"
 import EthereumWalletButton from "../EthereumWalletButton"
+import WalletModal from "../WalletModal/WalletModal"
 import TopBanner from "./TopBanner"
 
 function Header() {
   return (
     <>
       <TopBanner />
-      <header className="flex justify-center sm:justify-between px-8 py-4">
-
-        <h1 className="text-green-500 text-2xl font-normal pt-1">warp.green bridge</h1>
-
-        <div className="hidden sm:flex">
-          <EthereumWalletButton />
-          <ChiaWalletModal />
-        </div>
-
+      <header className="flex items-center justify-between px-4 sm:px-8 py-4">
+        <h1 className="text-green-500 text-xl sm:text-2xl font-normal pt-1">warp.green<br />bridge interface</h1>
+        <WalletModal />
       </header>
     </>
   )
