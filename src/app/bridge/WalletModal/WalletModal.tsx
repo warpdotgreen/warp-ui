9c679553238a82f6ed4df365e5de3e81d49e7256
Jonathan <jonathan@langtons.me>
2024-05-04 12:31:58 +0100

    Add button state for when chia wallet is connected (draft)

--- src/app/bridge/WalletModal/WalletModal.tsx
+++ src/app/bridge/WalletModal/WalletModal.tsx
@@ -1,3 +1,4 @@
+"use client"
 import {
   Dialog,
   DialogContent,
@@ -6,15 +7,25 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog"
+import { Avatar } from "@/components/ui/avatar"
 import ChiaWalletButtons from "../ChiaWalletButtons"
 import EthereumWalletButton from "../EthereumWalletButton"
 import { Button } from "@/components/ui/button"
-
+import { useWallet } from "../ChiaWalletManager/WalletContext"
 
 function WalletModal() {
+  const { walletConnected } = useWallet()
+  const buttonTrigger = !walletConnected
+    ?
+    <Button>Connect Wallet</Button>
+    :
+    <Avatar className="hover:opacity-80 bg-muted cursor-pointer"></Avatar>
+
   return (
     <Dialog>
-      <DialogTrigger asChild><Button>Connect Wallet</Button></DialogTrigger>
+      <DialogTrigger asChild>
+        {buttonTrigger}
+      </DialogTrigger>
       <DialogContent>
         <DialogHeader>
           <DialogTitle>Connect Wallet</DialogTitle>
