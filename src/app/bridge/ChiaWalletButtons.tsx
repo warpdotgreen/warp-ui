c341b986ca623240467784482733084f88255fdf
Jonathan <41968179+JonathanLangton1@users.noreply.github.com>
2024-05-06 10:28:56 +0100

    Change theming

--- src/app/bridge/ChiaWalletButtons.tsx
+++ src/app/bridge/ChiaWalletButtons.tsx
@@ -12,17 +12,18 @@ const ChiaWalletButtons: React.FC = () => {
   return (
     <div className='text-white'>
 
-      <div className='grid grid-cols-2 sm:grid-cols-3 gap-2'>
+      <div className='grid grid-cols-2 gap-2'>
         {walletConfigs.map(wallet => (
           <div key={wallet.id} className='relative flex flex-col flex-1 h-36 max-h-36 aspect-square w-full'>
             <Button
+              variant="theme"
               disabled={wallet.id === walletConnected}
-              className={cn('group w-full h-full disabled:opacity-100 disabled:bg-green-100 select-none', wallet.id === walletConnected && 'rounded-b-none')}
+              className={cn('group w-full h-full disabled:opacity-100 disabled:bg-theme-green select-none', wallet.id === walletConnected && 'rounded-b-none')}
               onClick={() => connectWallet(wallet.id)}
             >
-              <Image className='group-hover:opacity-80 max-h-24 h-full aspect-auto w-auto' src={wallet.icon} width={400} height={400} alt={`${wallet.name} wallet icon`} priority />
+              <Image className='group-hover:opacity-80 max-h-16 h-full aspect-auto w-auto' src={wallet.icon} width={400} height={400} alt={`${wallet.name} wallet icon`} priority />
             </Button>
-            <p className={cn('transition-all rounded-b-md px-1 py-1.5 text-xs font-medium select-none uppercase text-center w-full', wallet.id === walletConnected && 'bg-green-800')}>{wallet.id === walletConnected ? 'Connected' : wallet.name}</p>
+            <p className={cn('rounded-b-md px-1 py-1.5 text-xs font-medium select-none uppercase text-center w-full', wallet.id === walletConnected && 'bg-theme-green-foreground text-secondary font-semibold')}>{wallet.id === walletConnected ? 'Connected' : wallet.name}</p>
           </div>
         ))}
         {/* <Button onClick={disconnectWallet} disabled={!walletConnected}>Disconnect</Button> */}
