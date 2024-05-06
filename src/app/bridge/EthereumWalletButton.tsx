dfe9752aacf82e778d2e0574b328cf8afd9039c0
Jonathan <41968179+JonathanLangton1@users.noreply.github.com>
2024-05-06 10:29:16 +0100

    Replace default web3-modal button with a custom one to match design

--- src/app/bridge/EthereumWalletButton.tsx
+++ src/app/bridge/EthereumWalletButton.tsx
@@ -1,15 +1,21 @@
 "use client"
-
-declare global {
-  namespace JSX {
-    interface IntrinsicElements {
-      "w3m-button": any
-    }
-  }
-}
+import { Button } from '@/components/ui/button'
+import { cn } from '@/lib/utils'
+import { useWalletInfo, useWeb3Modal } from '@web3modal/wagmi/react'
+import Image from 'next/image'
 
 export default function EthereumWalletButton() {
+  const { open } = useWeb3Modal()
+  const { walletInfo } = useWalletInfo()
+  const isConnected = Boolean(walletInfo?.name)
   return (
-    <w3m-button balance='hide' label="Connect Ethereum Wallet" />
+    <div className='grid grid-cols-2 gap-2'>
+      <div className='relative flex flex-col flex-1 h-36 max-h-36 aspect-square w-full'>
+        <Button variant="theme" className={cn('group w-full h-full disabled:opacity-100 disabled:bg-theme-green select-none', isConnected && 'rounded-b-none')} onClick={() => open()}>
+          <Image className='group-hover:opacity-80 max-h-16 h-full aspect-auto w-auto' src="/icons/Walletconnect-icon.svg" width={400} height={400} alt={`Wallet connect icon`} priority />
+        </Button>
+        <p className={cn('rounded-b-md px-1 py-1.5 text-xs font-medium select-none uppercase text-center w-full', isConnected && 'bg-theme-green-foreground text-secondary font-semibold')}>{isConnected ? 'Connected' : ''}</p>
+      </div>
+    </div>
   )
 }
