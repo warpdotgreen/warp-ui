89574e5c51f21c9c7b1fda34102a3b85e689f209
Jonathan <41968179+JonathanLangton1@users.noreply.github.com>
2024-05-08 20:40:34 +0100

    Prevent layout shift on reload

--- src/app/bridge/components/Header.tsx
+++ src/app/bridge/components/Header.tsx
@@ -8,7 +8,7 @@ function Header() {
     <>
       <TopBanner />
       <header className="flex items-center justify-between px-4 sm:px-8 py-4">
-        <Image src="/warp-green-logo.png" className="h-auto w-36" alt="warp.green logo" width={837} height={281} />
+        <Image src="/warp-green-logo.png" className="h-auto aspect-auto w-36" alt="warp.green logo" width={837} height={281} />
         <div className="flex gap-2">
           <EthereumWalletButton />
           <WalletModal />
