52f50ae2819b9705d04d096299a448c031587b8f
Jonathan <41968179+JonathanLangton1@users.noreply.github.com>
2024-05-08 18:47:11 +0100

    Created top level eth wallet button (web3modal)

--- src/app/bridge/EthereumWalletButton.tsx
+++ src/app/bridge/EthereumWalletButton.tsx
@@ -2,20 +2,16 @@
 import { Button } from '@/components/ui/button'
 import { cn } from '@/lib/utils'
 import { useWalletInfo, useWeb3Modal } from '@web3modal/wagmi/react'
-import Image from 'next/image'
 
 export default function EthereumWalletButton() {
   const { open } = useWeb3Modal()
   const { walletInfo } = useWalletInfo()
   const isConnected = Boolean(walletInfo?.name)
   return (
-    <div className='grid grid-cols-2 gap-2'>
-      <div className='relative flex flex-col flex-1 h-36 max-h-36 aspect-square w-full'>
-        <Button variant="theme" className={cn('group w-full h-full disabled:opacity-100 disabled:bg-theme-green select-none', isConnected && 'rounded-b-none')} onClick={() => open()}>
-          <Image className='group-hover:opacity-80 max-h-16 h-full aspect-auto w-auto' src="/icons/Walletconnect-icon.svg" width={400} height={400} alt={`Wallet connect icon`} priority />
-        </Button>
-        <p className={cn('rounded-b-md px-1 py-1.5 text-xs font-medium select-none uppercase text-center w-full', isConnected && 'bg-theme-green-foreground text-secondary font-semibold')}>{isConnected ? 'Connected' : ''}</p>
-      </div>
-    </div>
+    <Button onClick={() => open()} variant="outline" className={cn("text-base gap-2 hidden sm:flex", !isConnected && 'shadow-sm shadow-green-300 grayscale')}>
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      {(isConnected && !!walletInfo?.icon) && <img className='group-hover:opacity-80 h-6 shadow-sm shadow-white/50 border rounded-full aspect-square bg-accent p-1 object-contain w-auto' src={walletInfo.icon} alt="ETH Connected Wallet Icon" />}
+      {isConnected ? 'Manage ETH Wallet' : 'Connect ETH Wallet'}
+    </Button>
   )
 }
