bf7ed4409b80b4658837721164c1e5e1f4db5c4b
Jonathan <41968179+JonathanLangton1@users.noreply.github.com>
2024-05-08 20:16:50 +0100

    Added updated wallet buttons

--- src/app/bridge/components/Header.tsx
+++ src/app/bridge/components/Header.tsx
@@ -1,14 +1,18 @@
 import Image from "next/image"
 import WalletModal from "../WalletModal/WalletModal"
 import TopBanner from "./TopBanner"
+import EthereumWalletButton from "../EthereumWalletButton"
 
 function Header() {
   return (
     <>
       <TopBanner />
       <header className="flex items-center justify-between px-4 sm:px-8 py-4">
-        <Image src="/warp-green-logo.png" className="h-24 w-auto" alt="warp.green logo" width={837} height={281} />
-        <WalletModal />
+        <Image src="/warp-green-logo.png" className="h-auto w-36" alt="warp.green logo" width={837} height={281} />
+        <div className="flex gap-2">
+          <EthereumWalletButton />
+          <WalletModal />
+        </div>
       </header>
     </>
   )
