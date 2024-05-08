f42bdcc3ae7ac3c17e9c13592a576196db73fe66
yakuhito <y@kuhi.to>
2024-05-08 13:30:27 +0300

    fix sig stuff

--- src/app/bridge/drivers/portal.tsx
+++ src/app/bridge/drivers/portal.tsx
@@ -315,6 +315,7 @@ export async function getSigsAndSelectors(
   pool.close(NOSTR_CONFIG.relays);
 
   if(events.length === 0) {
+    console.log("No Nostr events found for this nonce (yet)"); // todo: debug
     return [[], []];
   }
 
@@ -323,7 +324,9 @@ export async function getSigsAndSelectors(
     const destinationNetworkId = hexToString(destinationChainHex);
     const destinationNetwork = NETWORKS.filter((network) => network.id === destinationNetworkId)[0];
 
-    const sigStrings = events.sort((a, b) => {
+    const sigStrings = events.filter((e) => 
+      NOSTR_CONFIG.validatorKeys.includes(e.pubkey)
+    ).sort((a, b) => {
         const indexA = NOSTR_CONFIG.validatorKeys.findIndex(key => key === a.pubkey);
         const indexB = NOSTR_CONFIG.validatorKeys.findIndex(key => key === b.pubkey);
 
