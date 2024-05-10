5d8a891889ec728d62d16c00e2a204cee95fb5e9
Jonathan <41968179+JonathanLangton1@users.noreply.github.com>
2024-05-10 17:32:10 +0100

    Make top banner pretty

--- src/app/bridge/components/TopBanner.tsx
+++ src/app/bridge/components/TopBanner.tsx
@@ -4,8 +4,8 @@ function TopBanner() {
 
   if (TESTNET) {
     return (
-      <div className="text-yellow-300 bg-destructive sm:text-center w-full py-2 px-4 font-medium">
-        <p>This is a testnet interface. Do not use mainnet funds. Make sure Goby is on <span className="text-blue-800 dark:text-blue-300">testnet11</span> and your Ethereum wallet is on <span className="text-blue-800 dark:text-blue-300">Sepolia</span> or <span className="text-blue-800 dark:text-blue-300">Base Sepolia</span>.</p>
+      <div className="bg-destructive text-center w-full py-2 px-4 font-light">
+        <p>This is a testnet interface. Do not use mainnet funds. Make sure Goby is on <span className="bg-theme-purple rounded-sm px-2 whitespace-nowrap">testnet11</span> and your Ethereum wallet is on <span className="bg-theme-purple rounded-sm px-2 whitespace-nowrap">Sepolia</span> or <span className="bg-theme-purple rounded-sm px-2 whitespace-nowrap">Base Sepolia</span>.</p>
       </div>
     )
   }
