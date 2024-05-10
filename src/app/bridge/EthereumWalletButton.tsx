1e431619393669574d13931432fe6dc0649fddcc
Jonathan <41968179+JonathanLangton1@users.noreply.github.com>
2024-05-10 17:37:38 +0100

    Get rid of greyscale bc it's annoying for focus

--- src/app/bridge/EthereumWalletButton.tsx
+++ src/app/bridge/EthereumWalletButton.tsx
@@ -10,7 +10,7 @@ export default function EthereumWalletButton() {
   const account = useAccount()
   const isConnected = Boolean(walletInfo?.name)
   return (
-    <Button onClick={() => open()} variant="outline" className={cn("text-base gap-2 flex", !isConnected && 'shadow-sm shadow-green-300 grayscale')}>
+    <Button onClick={() => open()} variant="outline" className={cn("text-base gap-2 flex", !isConnected && 'shadow-sm shadow-white/80')}>
       {/* eslint-disable-next-line @next/next/no-img-element */}
       {(isConnected && !!walletInfo?.icon) && <img className='group-hover:opacity-80 h-6 shadow-sm shadow-white/50 border rounded-full aspect-square bg-accent p-1 object-contain w-auto' src={walletInfo.icon} alt="ETH Connected Wallet Icon" />}
       {isConnected ? !!account.address ? `${account.address.slice(0, 5)}...${account.address.slice(-3)}` : 'Manage Wallet' : 'Connect ETH Wallet'}
