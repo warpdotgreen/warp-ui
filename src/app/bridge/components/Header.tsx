83005bfa9742148170db33c5c44113bc2014c435
Jonathan <41968179+JonathanLangton1@users.noreply.github.com>
2024-05-11 12:15:23 +0100

    warp.green Bridging Interface title on the top left

--- src/app/bridge/components/Header.tsx
+++ src/app/bridge/components/Header.tsx
@@ -5,13 +5,17 @@ import TopBanner from "./TopBanner"
 import EthereumWalletButton from "../EthereumWalletButton"
 import MobileNav from "./Nav/MovileNav"
 import DesktopNav from "./Nav/DesktopNav"
+import Link from "next/link"
 
 function Header() {
   return (
     <>
       <TopBanner />
       <header className="grid grid-cols-2 xl:grid-cols-3 items-center px-4 sm:px-8 py-4">
-        <Image src="/warp-green-logo.png" className="h-auto aspect-auto w-36" alt="warp.green logo" width={837} height={281} />
+        <Link href="/" className="select-none hover:opacity-80 w-fit transition-colors focus-visible:outline-none ring-offset-accent focus-visible:ring-2 rounded focus-visible:ring-ring focus-visible:ring-offset-2">
+          <Image src="/warp-green-logo.png" className="h-auto aspect-auto w-36" alt="warp.green logo" width={837} height={281} />
+          <p className="text-sm text-theme-purple">Bridge Interface</p>
+        </Link>
 
         <div className="xl:hidden flex justify-end">
           <MobileNav />
