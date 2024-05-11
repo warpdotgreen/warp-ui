5daf025847f47be365d215d10fcda8e32d42fad2
Jonathan <41968179+JonathanLangton1@users.noreply.github.com>
2024-05-11 15:16:05 +0100

    Make logo foxed height and make priority to prevent layout shift & flicker

--- src/app/bridge/components/Header.tsx
+++ src/app/bridge/components/Header.tsx
@@ -13,7 +13,7 @@ function Header() {
       <TopBanner />
       <header className="grid grid-cols-2 xl:grid-cols-3 items-center px-4 sm:px-8 py-4">
         <Link href="/" className="select-none hover:opacity-80 w-fit transition-colors focus-visible:outline-none ring-offset-accent focus-visible:ring-2 rounded focus-visible:ring-ring focus-visible:ring-offset-2">
-          <Image src="/warp-green-logo.png" className="h-auto aspect-auto w-36" alt="warp.green logo" width={837} height={281} />
+          <Image src="/warp-green-logo.png" className="h-5 aspect-auto w-auto" alt="warp.green logo" width={837} height={281} priority />
           <p className="text-sm text-theme-purple">Bridge Interface</p>
         </Link>
 
