142f24e89957005621ea3df55563c44437d082a1
Jonathan <41968179+JonathanLangton1@users.noreply.github.com>
2024-05-11 12:21:50 +0100

    bottom of the page: change to 'powered by warp.green' & add some bottom padding/margin

--- src/app/bridge/components/Footer.tsx
+++ src/app/bridge/components/Footer.tsx
@@ -3,10 +3,11 @@ import Link from "next/link"
 
 function Footer() {
   return (
-    <div className="flex p-4 justify-center text-muted-foreground pb-3">
-      <div className="flex flex-wrap justify-center text-sm">
-        <Link href="https://warp.green" className="text-theme-green-foreground hover:text-green-300 font-medium hover:underline">
-          <Image src="/warp-green-logo.png" className="h-4 aspect-auto w-auto" alt="warp.green logo" width={837} height={281} />
+    <div className="flex px-4 pt-12 pb-4 justify-center text-muted-foreground">
+      <div className="flex flex-wrap justify-center gap-1 items-center">
+        <p className="text-theme-purple">Powered By</p>
+        <Link href="/" className="text-theme-green-foreground hover:opacity-80 hover:text-green-300 font-medium hover:underline transition-colors focus-visible:outline-none ring-offset-accent rounded-sm focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2">
+          <Image src="/warp-green-logo.png" className="h-3.5 aspect-auto w-auto" alt="warp.green logo" width={837} height={281} />
         </Link>
       </div>
     </div>
