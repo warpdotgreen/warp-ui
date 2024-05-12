1199b58777eb4cbef1a9df19ef7c98f4b35b8497
Jonathan <41968179+JonathanLangton1@users.noreply.github.com>
2024-05-12 18:02:03 +0100

    Make 'powered by' lowercase

--- src/app/bridge/components/Footer.tsx
+++ src/app/bridge/components/Footer.tsx
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <div className="flex px-4 pt-12 mt-auto pb-4 justify-center text-muted-foreground">
       <div className="flex flex-wrap justify-center gap-1 items-center">
-        <p className="text-theme-purple">Powered By</p>
+        <p className="text-theme-purple lowercase">Powered By</p>
         <Link href="/" className="text-theme-green-foreground hover:opacity-80 hover:text-green-300 font-medium hover:underline transition-colors focus-visible:outline-none ring-offset-accent rounded-sm focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2">
           <Image src="/warp-green-logo.png" className="h-3.5 aspect-auto w-auto" alt="warp.green logo" width={837} height={281} />
         </Link>
