39a4d8fc6c70d400c2e940065b599f45264013b0
Jonathan <41968179+JonathanLangton1@users.noreply.github.com>
2024-05-13 21:54:05 +0100

    Add tooltips on steps 1 and 3 saying 'This step includes a transaction'

--- src/app/bridge/MultiStepForm.tsx
+++ src/app/bridge/MultiStepForm.tsx
@@ -2,6 +2,7 @@ import { Button } from "@/components/ui/button"
 import { NetworkType, type Network } from "./config"
 import { ChevronLeft } from "lucide-react"
 import Link from "next/link"
+import { withToolTip } from "@/lib/utils"
 
 export function MultiStepForm({
   sourceChain,
@@ -29,15 +30,16 @@ export function MultiStepForm({
               <div className={`w-full rounded-lg p-2 transition-all border bg-accent ${activeStep - 1 == index ? '' : 'opacity-50'}`}>
                 <div className="flex flex-col-reverse sm:flex-row gap-2 items-start justify-between sm:items-center p-4">
                   <p className="text-xl font-light">{index + 1}. {step.text}</p>
-                  <p className="px-2 rounded-full bg-theme-purple font-light">{step.iconText}</p>
+                  {index === 1 && <p className="px-2 rounded-full bg-theme-purple font-light">{step.iconText}</p>}
                   {index === 0 && activeStep === 1 && (
-                    <Button variant="outline" className="w-full sm:w-fit" asChild>
+                    <Button variant="outline" className="w-full mb-4 sm:mb-0 sm:w-fit ml-auto mr-2" asChild>
                       <Link href="/bridge">
                         <ChevronLeft className="w-4 h-auto -ml-1 mr-1" />
                         Back
                       </Link>
                     </Button>
                   )}
+                  {(index === 2 || index === 0) && <div className="hidden sm:flex">{withToolTip(<div className="group-hover:opacity-80 h-5 justify-center items-center shadow-sm shadow-white/50 border rounded-full aspect-square bg-accent text-sm font-normal text-primary/80 w-auto">?</div>, 'This Step Includes a Transaction')}</div>}
                 </div>
                 <div>
                   {activeStep - 1 == index && (
