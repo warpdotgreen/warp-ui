a441b8189c9f3c6bc416cf6772aa83b80c1dbe2b
Jonathan <41968179+JonathanLangton1@users.noreply.github.com>
2024-05-13 15:56:18 +0100

    Add skip to navigation link

--- src/app/bridge/layout.tsx
+++ src/app/bridge/layout.tsx
@@ -2,6 +2,7 @@ import { ClientProvider } from "./ClientProvider"
 import { Suspense } from "react"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
+import { Button } from "@/components/ui/button"
 
 export default function BridgeUILayout({
   children,
@@ -11,9 +12,14 @@ export default function BridgeUILayout({
   return (
     <ClientProvider>
       <div className="bg-black min-h-screen flex flex-col">
+        <Button variant="outline" className="absolute left-0 backdrop-blur -translate-y-16 focus:translate-y-4 focus:translate-x-4" asChild>
+          <a href="#main-content">Skip Navigation</a>
+        </Button>
         <Header />
         <Suspense>
-          {children}
+          <main id="main-content" className="flex flex-col items-center h-full grow mx-auto transition-colors focus-visible:outline-none ring-offset-accent rounded-md focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2">
+            {children}
+          </main>
         </Suspense>
 
         <Footer />
