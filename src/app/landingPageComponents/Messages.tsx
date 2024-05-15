3ee3c65c3ee3ecebfdd40a7f0f6d87de97c9126a
Jonathan <41968179+JonathanLangton1@users.noreply.github.com>
2024-05-15 17:43:44 +0100

    Add arrow between chains

--- src/app/landingPageComponents/Messages.tsx
+++ src/app/landingPageComponents/Messages.tsx
@@ -5,6 +5,7 @@ import { NETWORKS } from "../bridge/config"
 import { cn, getChainIcon } from "@/lib/utils"
 import { Skeleton } from "@/components/ui/skeleton"
 import { CopyableLongHexString } from "@/components/CopyableHexString"
+import { ChevronRight } from "lucide-react"
 
 
 interface MessageResponse {
@@ -54,15 +55,16 @@ function Messages() {
     const sourceChainDisplayName = NETWORKS.find(n => n.id === m.source_chain)!.displayName
     const destChainDisplayName = NETWORKS.find(n => n.id === m.destination_chain)!.displayName
     const timeSince = formatDistanceToNow(timestamp * 1000, { addSuffix: true }).replace('about ', '')
-    const sourceChainIcon = <div className="border bg-background p-0.5 rounded-full translate-x-4">{getChainIcon(sourceChainDisplayName, 'w-5')}</div>
+    const sourceChainIcon = <div className="border bg-background p-0.5 rounded-full">{getChainIcon(sourceChainDisplayName, 'w-5')}</div>
     const destChainIcon = <div className="border z-10 bg-background p-0.5 rounded-full shadow">{getChainIcon(destChainDisplayName, 'w-5')}</div>
 
     return (
       <div key={m.id} className={cn("rounded-md w-full p-6 bg-background z-10 animate-in fade-in slide-in-from-bottom-2 duration-500 flex flex-col gap-2", i + 1 > 3 && 'hidden md:flex')}>
         <div className="flex gap-4 justify-between items-center">
           <CopyableLongHexString hexString={`0x${m.nonce}`} className="bg-transparent p-0 text-xl text-primary opacity-100" tooltipText="Copy Message Nonce" />
-          <div className="flex gap-2 py-2">
+          <div className="flex gap-1 py-2">
             {sourceChainIcon}
+            <ArrowRight />
             {destChainIcon}
           </div>
         </div>
@@ -99,3 +101,9 @@ function Messages() {
 }
 
 export default Messages
+
+function ArrowRight() {
+  return (
+    <svg className="opacity-80 w-4 h-auto" width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path></svg>
+  )
+}
