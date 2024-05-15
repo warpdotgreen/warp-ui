07ce30e16122e9dee3b1b9b924dc843a4ae29822
Jonathan <41968179+JonathanLangton1@users.noreply.github.com>
2024-05-15 18:18:47 +0100

    Disable star animation speed if user prefers reduced-motion & add messages delivered card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,30 @@ import Header from "./landingPageComponents/Header"
 import Link from "next/link"
 import Messages from "./landingPageComponents/Messages"
 import Starfield from "./landingPageComponents/Starfield"
+import { NETWORKS } from "./bridge/config"
+import { getChainIcon } from "@/lib/utils"
+import { useEffect, useState } from "react"
 
 export default function LandingPage() {
+
+  // Disable star animation speed if user prefers reduced-motion
+  const [isReducedMotion, setIsReducedMotion] = useState<boolean>(false)
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(prefers-reduced-motion: reduce)')
+    mediaQuery.addEventListener('change', () => {
+      console.log(mediaQuery.media, mediaQuery.matches)
+      setIsReducedMotion(true)
+    })
+  }, [])
+  const starSpeed = isReducedMotion ? 0 : 0.05
+
   return (
     <>
       <Header />
       <Starfield
         starCount={1000}
-        starColor={[255, 255, 255]} //[128, 100, 221]
-        speedFactor={0.05}
+        starColor={[255, 255, 255]}
+        speedFactor={starSpeed}
         backgroundColor="black"
       />
       <main id="scrollContainer" className="relative px-4 sm:px-8 py-4">
@@ -30,25 +45,29 @@ export default function LandingPage() {
               </div>
             </div>
 
-            {/* <div className="flex flex-wrap gap-4 justify-center my-12 grayscale">
-              {NETWORKS.map(n => (
-                <div key={n.id} className="border rounded-full p-4 shadow-white shadow-sm flex items-center justify-center bg-accent">{getChainIcon(n.displayName, "w-12")}</div>
-              ))}
-            </div> */}
-
             {/* <h3 className="text-xl mb-4 font-light opacity-80 mt-24">Messages</h3> */}
             <div className="bg-accent border rounded-lg p-2 w-full mt-auto animate-in fade-in duration-500 grid grid-cols-1 md:grid-cols-2 gap-2">
               <Messages />
             </div>
 
-            {/* <div className="bg-accent border rounded-lg p-6 w-full mt-4 flex flex-col sm:flex-row gap-4">
-              <div className="flex flex-col justify-center grow w-1/2">
-                <h3 className="text-xl font-light">Latest Messages</h3>
+            <div className="flex flex-wrap gap-4 justify-center my-12">
+              {NETWORKS.map(n => (
+                <div key={n.id} className="border rounded-full p-4 shadow-white shadow-sm flex items-center justify-center bg-accent">{getChainIcon(n.displayName, "w-12")}</div>
+              ))}
+            </div>
+
+            <div className="grid grid-cols-1 sm:grid-cols-3 w-full">
+
+              <div className="z-10 bg-accent border rounded-md flex gap-4 p-2 justify-between overflow-hidden">
+                <div className="text-xl self-center px-4">
+                  Messages Delivered
+                </div>
+                <div className="translate-y-8 px-4">
+                  <p className="text-9xl font-light">61</p>
+                </div>
               </div>
-              <div className="grid grid-cols-1 gap-2 justify-center animate-pulse w-full">
-                <Messages />
-              </div>
-            </div> */}
+
+            </div>
 
           </div>
         </section>
