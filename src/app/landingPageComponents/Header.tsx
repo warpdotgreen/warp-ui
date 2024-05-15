b72f4fb84113eb37cd373b0ee14ac929774146a1
Jonathan <41968179+JonathanLangton1@users.noreply.github.com>
2024-05-15 17:35:23 +0100

    Make header sticky

--- src/app/landingPageComponents/Header.tsx
+++ src/app/landingPageComponents/Header.tsx
@@ -5,17 +5,15 @@ import Link from "next/link"
 
 function Header() {
   return (
-    <>
-      <header className="flex justify-between items-center px-4 sm:px-8 py-4">
-        <Link href="/" className="select-none hover:opacity-80 w-fit transition-colors focus-visible:outline-none ring-offset-accent focus-visible:ring-2 rounded focus-visible:ring-ring focus-visible:ring-offset-2">
-          <Image src="/warp-green-logo.png" className="h-5 aspect-auto w-auto" alt="warp.green logo" width={837} height={281} priority />
-        </Link>
+    <header className="flex justify-between items-center px-4 sm:px-8 py-4 sticky top-0 bg-background/70 backdrop-blur z-20">
+      <Link href="/" className="select-none hover:opacity-80 w-fit transition-colors focus-visible:outline-none ring-offset-accent focus-visible:ring-2 rounded focus-visible:ring-ring focus-visible:ring-offset-2">
+        <Image src="/warp-green-logo.png" className="h-5 aspect-auto w-auto" alt="warp.green logo" width={837} height={281} priority />
+      </Link>
 
-        <Button variant="outline" className="" asChild>
-          <Link href="/bridge">Bridge Interface</Link>
-        </Button>
-      </header>
-    </>
+      <Button variant="outline" className="" asChild>
+        <Link href="/bridge">Bridge Interface</Link>
+      </Button>
+    </header>
   )
 }
 
