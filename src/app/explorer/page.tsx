0721db01beb76f46731f2c31ae849db0d87d39cb
yakuhito <y@kuhi.to>
2024-05-28 12:53:41 +0300

    mobile kinda works

--- src/app/explorer/page.tsx
+++ src/app/explorer/page.tsx
@@ -20,7 +20,7 @@ export function MessageSection({
   sent: boolean
 }) {
   return (
-    <div className="max-w-7xl mx-auto px-8">
+    <div className="max-w-7xl min-w-3xl mx-auto px-8 overflow-x-auto">
       <div className="mt-4 text-xl">{sent ? 'Sent' : 'Received'} Messages</div>
       <div className="flow-root px-4">
         <table className="min-w-full divide-y divide-zinc-900 mt-4">
